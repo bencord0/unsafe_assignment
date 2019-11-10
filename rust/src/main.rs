50d85770dbbfdc3c51f9b5ad38baee4448793939
Ben Cordero <bencord0@condi.me>
2019-11-10 16:47:17 +0000

    rust: Use async-std
    - Swap std::thread for async_std::task
    - Use async blocks, block_on and join!
    - Remove unwrap() calls.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,32 +1,35 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
+#![type_length_limit="2873418"]
+
+use async_macros::join;
+use async_std::sync::Mutex;
+use async_std::task;
+use std::sync::Arc;
 use std::time::Duration;
 
 use rand::random;
 
-fn maybe_sleep() {
+async fn maybe_sleep() {
     if random::<bool>() {
-        thread::sleep(Duration::from_secs(1));
+        task::sleep(Duration::from_secs(1)).await;
     }
 }
 
 fn main() {
-    let a = Arc::new(Mutex::new(0));
+    let a = Arc::new(Mutex::new(0u8));
     let a_1 = a.clone();
     let a_2 = a.clone();
 
-    let t1 = thread::spawn(move || {
-        maybe_sleep();
-        *a_1.lock().unwrap() = 1;
+    let t1 = async {
+        maybe_sleep().await;
+        *a_1.lock().await = 1;
+    };
+    let t2 = async {
+        maybe_sleep().await;
+        *a_2.lock().await = 2;
+    };
+
+    task::block_on(async {
+        join!(t1, t2).await;
+        println!("{:?}", a.lock().await);
     });
-    let t2 = thread::spawn(move || {
-        maybe_sleep();
-        *a_2.lock().unwrap() = 2;
-    });
-
-    let _ = t1.join();
-    let _ = t2.join();
-
-    println!("{:?}", a.lock().unwrap());
-
 }
